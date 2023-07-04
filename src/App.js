08755f5aa7f3aea8246e594f94ba01bb3f931acd
Adil Ahamad <adilahamad.personally@gmail.com>
2023-07-04 12:28:15 +0530

    commit other changes.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import About from "./pages/about/About";
+import Contect from "./pages/contect/Contect";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/login";
+import Product from "./pages/product/Product";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: true ? <Layout /> : <p>Please Authenticate!</p>,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        children: [{
+          path: "/home/:id",
+          element: <Home />
+        }]
+      },
+      {
+        path: "/product",
+        element: <Product />,
+      },
+      {
+        path: "/contect",
+        element: <Contect />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "*",
+    element: <h1>Page not found!</h1>,
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">
+    <RouterProvider router={router} />
+  </div>;
 }
 
 export default App;
